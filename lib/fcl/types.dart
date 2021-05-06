28ad2d922a5e0d4a54c1dcd27513f0f15636f64c
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-06 03:05:40 +0300

    Add basic CadenceValue type

--- lib/fcl/types.dart
+++ lib/fcl/types.dart
@@ -1,6 +1,39 @@
 import 'dart:convert';
+import 'package:mini_flow/fcl/utils.dart';
 
 // TODO: Reformat to CadenceValue similar to Go
+
+enum CadenceType {
+  String,
+  Int,
+  Int8,
+  UInt,
+  UFix64,
+}
+
+class CadenceValue {
+  final CadenceType type;
+  dynamic value;
+
+  CadenceValue({this.value, this.type});
+
+  CadenceValue.fromJson(Map<String, dynamic> json)
+      : type = enumFromString(CadenceType.values, json['type']),
+      value = json['value'];
+
+  Map<String, dynamic> toJson() => {
+    'type': type.toString().split('.').last,
+    'value': value
+  };
+
+  String toJsonString() {
+    return json.encode(this.toJson());
+  }
+
+  List<int> toMessage(){
+    return utf8.encode(this.toJsonString());
+  }
+}
 
 /// Representation of Cadence Int
 class CadenceInt {
